448aff28f2a0558373cbd70d0b0fe8b8d1e5f408
[Ahmet Albayrak] <[ahmetalperenalbayrak@gmail.com]>
2022-01-25 22:02:03 +0300

    Final adjustments made

---
+++ Expanding Cards/app.js
@@ -0,0 +1,18 @@
+// // with queryselectorAll we create a node list. Just like arrays//
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+const panel = document.querySelectorAll('.panel')
+
