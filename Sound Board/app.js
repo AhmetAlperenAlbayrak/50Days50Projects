a0fef3c9bde2af00f0b59e1a422833cdf58eeefe
[Ahmet Albayrak] <[ahmetalperenalbayrak@gmail.com]>
2022-01-31 21:13:14 +0300

    Nature Sound Board Added

---
+++ Sound Board/app.js
@@ -0,0 +1,23 @@
+const sounds = ['birds', 'wolves', 'rain', 'volcano','storm' ,'wind']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+        document.getElementById(sound).onplay()
+    })
+
+    document.getElementById('buttons').appendChild(btn);
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        sound.pause()
+        song.currentTime = 0;
+    })
+}
